da911af7fd8c4ceb8481c7ffbc417d8595d647eb
Mayank Verma <iamironman029@gmail.com>
2026-06-25 23:14:25 +0530

    Time: 69 ms (96.04%), Space: 20.8 MB (24.12%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -7,23 +7,29 @@ class Solution(object):
         size = len(height)
         left =  0
         right = size - 1
-        area_list = [0] * size
-        pos = size - 1
+        #area_list = [0] * size
+        area_list = 0
+        #pos = size - 1
         while left <  right and pos >= 0:
             if height[left] >= height[right]:
                 length = height[right]
                 breadth = right - left
                 area = length * breadth
-                area_list[pos] = area
+                #area_list[pos] = area
+                if area_list < area:
+                    area_list = area
                 right = right - 1
-                pos = pos - 1
+                #pos = pos - 1
             elif height[right] >= height[left]:
                 length = height[left]
                 breadth = right - left
                 area = length * breadth
-                area_list[pos] = area
+                if area_list < area:
+                    area_list = area
+                #area_list[pos] = area
                 left = left + 1
-                pos = pos - 1
-        area_list.sort()
-        return area_list[size - 1]         
-
+                #pos = pos - 1
+        #area_list.sort()
+        
+        #return area_list[size - 1]         
+        return area_list
