7801698e9959023199d09e3c6acd05f6bed96269
Mayank Verma <iamironman029@gmail.com>
2026-06-27 19:01:33 +0530

    Time: 22 ms (8.87%), Space: 13.7 MB (39.35%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        left = 0
+        right = 1
+        counter = 1
+        if len(nums) == 1:
+            return nums[0]
+
+        else:
+            while right < len(nums) and left < len(nums):
+                if nums[right] == nums[left]:
+                    counter += 1
+                    if counter > len(nums) / 2:
+                        return nums[left]
+                    else:
+                        right = right + 1
+                elif nums[right] != nums[left]:
+                    if counter > len(nums) / 2:
+                       return nums[left]
+                    else:
+                        left = right
+                        right = right + 1
+                        counter = 1
+
