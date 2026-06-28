b16c0a7378f1054dfe79c9b0947fdc9d815e89d4
Mayank Verma <iamironman029@gmail.com>
2026-06-28 17:22:50 +0530

    Time: 41 ms (13.43%), Space: 28.6 MB (6.25%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,13 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        nums.sort()
-        left = 0
-        right = 1
-        while right < len(nums):
-            if nums[left] != nums[right]:
-                left += 1
-                right += 1
+        #nums.sort()
+        #left = 0
+        #right = 1
+        #while right < len(nums):
+            #if nums[left] != nums[right]:
+                #left += 1
+                #right += 1
+            #else:
+                #return True    
+        #return False
+        freq  = {}
+        i = 0
+        while i < len(nums):
+            if nums[i] in freq:
+                return True
             else:
-                return True    
-        return False        
+                freq[nums[i]] = 1
+                i = i + 1
+        return False            
