9f41f48f4bdae32bd820d30ac9e38641f47bbe69
Mayank Verma <iamironman029@gmail.com>
2026-08-15 19:56:43 +0530

    Time: 0 ms (100%), Space: 19.2 MB (60.75%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -3,11 +3,8 @@ class Solution:
         if n == 0:
             return 0
         else:
-            fact = 1
             count = 0
-            for i in range (n, 0,-1):
-                fact *= i
-        while fact % 10 == 0:
-            count += 1
-            fact = fact // 10
-        return count    
+            while n > 0:
+                n = n // 5
+                count += n
+            return count       
