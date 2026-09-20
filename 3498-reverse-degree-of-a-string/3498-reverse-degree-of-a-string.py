a6151814f81c706b7b6cdb4b632cca097baba681
Mayank Verma <iamironman029@gmail.com>
2026-09-20 22:53:09 +0530

    Time: 9 ms (27.39%), Space: 19.3 MB (55.57%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        total_degree = 0 
+        for i, char in enumerate(s, start=1):
+            rev_alphabet_pos = 26 - (ord(char) - ord('a'))
+            total_degree += rev_alphabet_pos * i  
+        return total_degree
